9fced19c39e62e691e1162695ba532b99258e640
zocha_sz <zofia.szczudlik@gmail.com>
2017-07-21 21:39:34 +0200

    Home page in progress

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,30 @@
 const express = require('express');
 const router = express.Router();
 const TYPES = require('../models/product-types');
+const Event = require('../models/event');
 
-router.get('/', function(req, res, next) {
-  res.render('index', { TYPES });
+router.get('/', (req, res, next) => {
+  const events = Event.find({products: "Eggs"});
+  console.log(events);
+  res.render('index', { TYPES, events });
 });
+
+// router.get('/', (req, res, next) => {
+//   Event
+//     .find({})
+//     .populate('_creator')
+//     .exec( (err, events) => {
+//       console.log(events);
+//       res.render('index', { TYPES });
+//     });
+// });
+
+// router.post('/', (req, res, next) => {
+//   const events = Event.find({})
+//       console.log(events);
+//       res.render('index', { events });
+//     });
+// });
+
 
 module.exports = router;
---
+++ routes/producer.js
@@ -0,0 +1,9 @@
+const express = require('express');
+const router = express.Router();
+const TYPES = require('../models/product-types');
+
+router.get('/', function(req, res, next) {
+  res.render('index');
+});
+
+module.exports = router;
