df5d3c230519a3cd5cbdb0ad8d2f02e2934892aa
Joseba Irujo <joseiru@gmail.com>
2017-07-27 22:44:03 +0200

    Session management

--- public/javascripts/scripts.js
+++ public/javascripts/scripts.js
@@ -1,16 +1,3 @@
 $( "#toggle-producer" ).change(function() {
     $( "#form-producer" ).toggle();
 });
-
-function getGeolocationCoordinates(id) {
-  $.ajax({
-    url: "http://pokeapi.co/api/v2/pokemon/" + id,
-    method: "GET",
-    success: function (response) {
-      console.log(response);
-    },
-    error: function (err) {
-      console.log(err);
-    },
-  })
-}
